1d1695e5bf2a8c782b6aafb135dfdac0e33048ca
pedrotiussi <pedrotiussi@github.com>
2020-06-15 21:42:17 -0300

    correções no back-end para que retorne a imagem correta do banco de dados

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # nlw
+# Projeto feito por Pedro Tiussi com a monitoria da Rocketseat no evento da NLW1
+
+
+Esse é um projeto didático desenvolvido pela Rocketseat na NextLevelWeek de julho de 2020.
+Será desenvolvida uma API RESTful em TypeScript como o NodeJS para o back-end e React (web) e React Native (mobile) para o front-end.
+A inteção é que empresas possam se cadastrar como ponto de coleta de materaias recicláveis e que as pessoas possam ter acesso a este tipo
+de informação para retornar esses produtos de forma segura.
--- server/src/database/seeds/default_items.ts
+++ server/src/database/seeds/default_items.ts
@@ -2,11 +2,11 @@ import Knex from 'knex'
 
 export async function seed (knex: Knex){
     await knex('items').insert([
-        {title: 'Lâmpadas', image: 'lampas.sgv'},
-        {title: 'Pilhas e Baterias', image: 'baterias.sgv'},
-        {title: 'Papeis e Papelão', image: 'papeis-papelao.sgv'},
-        {title: 'Resíduos Eletrônicos', image: 'eletronicos.sgv'},
-        {title: 'Resíduos Orgânicos', image: 'organicos.sgv'},
-        {title: 'Óleo de Cozinha', image: 'oleo.sgv'},
+        {title: 'Lâmpadas', image: 'lampadas.svg'},
+        {title: 'Pilhas e Baterias', image: 'baterias.svg'},
+        {title: 'Papeis e Papelão', image: 'papeis-papelao.svg'},
+        {title: 'Resíduos Eletrônicos', image: 'eletronicos.svg'},
+        {title: 'Resíduos Orgânicos', image: 'organicos.svg'},
+        {title: 'Óleo de Cozinha', image: 'oleo.svg'},
     ]); 
 }
