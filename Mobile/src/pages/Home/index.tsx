f9602148af5af39193e00d3dd919f74aad9da850
pedrotiussi <pedrotiussi@github.com>
2020-06-19 12:44:54 -0300

    Criação da página Points. Nela o usuário consegue visualizar pontos de coleta de resíduos e pode navegar para a página Details (que será desenvolvida a posteriori)

--- Mobile/src/pages/Home/index.tsx
+++ Mobile/src/pages/Home/index.tsx
@@ -2,35 +2,43 @@ import React from 'react';
 import { View, ImageBackground, Image, StyleSheet, Text} from 'react-native';
 import { RectButton } from 'react-native-gesture-handler';
 import { Feather as Icon} from '@expo/vector-icons'
+import { useNavigation } from '@react-navigation/native';
+
 
 
 const Home = () => {
-    return (
-        <ImageBackground 
-        style={styles.container} 
-        source={require('../assets/home-background.png')}
-        imageStyle={{width: 274, height: 368 }}
-        >
-            <View style={styles.main}>
-                <Image source = {require('../assets/logo.png')} />
-                <Text style={styles.title}>Seu marketplace de coleta de resíduos</Text>
-                <Text style={styles.description}>Ajudamos pessoas a encontrarem pontos de coleta de forma eficiente</Text>
-            </View>
-            <View style={styles.footer}>
-                <RectButton style={styles.button} onPress= {()=> {}}> 
-                    <View style={styles.buttonIcon}>
-                       <Text >
-                            <Icon name="arrow-right" color= "#FFF" size= {24} />
-                        </Text>
-                    </View>
-                    <Text style={styles.buttonText}>
-                        Entrar
-                    </Text>
-                </RectButton>
-            </View>
+  const navigation = useNavigation();
 
-        </ImageBackground>
-    );
+  function handleNavigateToPoints (){
+    navigation.navigate('Points');
+  }
+
+  return (
+    <ImageBackground 
+    style={styles.container} 
+    source={require('../../assets/home-background.png')}
+    imageStyle={{width: 274, height: 368 }}
+    >
+      <View style={styles.main}>
+        <Image source = {require('../../assets/logo.png')} />
+        <Text style={styles.title}>Seu marketplace de coleta de resíduos</Text>
+        <Text style={styles.description}>Ajudamos pessoas a encontrarem pontos de coleta de forma eficiente</Text>
+      </View>
+      <View style={styles.footer}>
+        <RectButton style={styles.button} onPress= {handleNavigateToPoints}> 
+          <View style={styles.buttonIcon}>
+            <Text >
+              <Icon name="arrow-right" color= "#FFF" size= {24} />
+            </Text>
+          </View>
+          <Text style={styles.buttonText}>
+            Entrar
+          </Text>
+        </RectButton>
+      </View>
+
+    </ImageBackground>
+  );
 };
 
 const styles = StyleSheet.create({
@@ -101,5 +109,6 @@ const styles = StyleSheet.create({
       fontSize: 16,
     }
 });
+
 
 export default Home;
